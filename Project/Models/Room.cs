a2bb3f6eef728206d2fd8038745cbffabe8c8434
ben <bholsinger@gmail.com>
2019-07-15 08:28:46 -0600

    merge

--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -10,45 +10,42 @@ namespace CastleGrimtol.Project.Models
     public string Description { get; set; }
     public List<Item> Items { get; set; }
     public Dictionary<string, IRoom> Exits { get; set; }
+    public int Key { get; set; }
 
 
 
 
 
 
-    public string GetWelcomeLine()
+
+    //using printoptions because of exits
+    public void PrintOptions()
     {
-      return "Welcome to {Name}";
-      //Name is the name of the room
-      //this print should be accessed in gameservice
-      // to define what is printed in each different room
+      Console.WriteLine("There are a total of 4 rooms in this Castle.  Each room has between one or two doors.  The directions you can go are North, South, East and West.  Each room is a two dimention square.  The first room you are in has a key to a locked door.  It also has Two doors.  The locked door will get you into the other 2 rooms.  Choose wisely.");
+      Console.Write("type 'go north', 'go south', 'to west', or 'go east' to go east.  ");
+      foreach (var exit in Exits)
+      {
+        Console.Write("Current exits: " + exit.Key + " : ");
+      }
+
     }
-        /* 
-            public void PrintOptions()
-            {
-              Console.WriteLine("There are a total of 4 rooms in this Castle.  Each room has between one or two doors.  The directions you can go are North, South, East and West.  Each room is a two dimention square.  The first room you are in has a key to a locked door.  It also has Two doors.  The locked door will get you into the other 2 rooms.  Choose wisely.");
-              Console.Write("type 'go north', 'go south', 'to west', or 'go east' to go east.  ");
-              foreach (var exit in Exits)
-              {
-                Console.Write("Current exits: " + exit.Key + " : ");
-              }
-
-            }
 
 
-                    public IRoom GoToRoom(string dir)
-                    {
-                      Console.WriteLine("using method. going to : " + dir);
-                      if (Exits.ContainsKey(dir))
-                      {
-                        Console.WriteLine("exits should contain direction used  " + dir);
-                        Console.WriteLine(Exits[dir]);
-                        return Exits[dir];
-                      }
-                      Console.WriteLine("Invalid direction");
-                      return this;
-                    }
-                */
+    public IRoom GoToRoom(string dir)
+    {
+      //there is a go method already in GameService 
+
+      // Console.WriteLine("using method. going to : " + dir);
+      // if (Exits.ContainsKey(dir))
+      // {
+      //   Console.WriteLine("exits should contain direction used  " + dir);
+      //   Console.WriteLine(Exits[dir]);
+      //   return Exits[dir];
+      // }
+      // Console.WriteLine("Invalid direction");
+      // return this;
+    }
+
 
     public Room(string name, string description, int key)
     {
@@ -56,6 +53,7 @@ namespace CastleGrimtol.Project.Models
       Description = description;
       Exits = new Dictionary<string, IRoom>();
       Items = new List<Item>();
+      Key = key;
     }
 
 
